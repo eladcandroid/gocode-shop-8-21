e41235cfe549181a78dc3b289488f8d017ed62d6
Elad Cohen <elad@codicate.com>
2021-08-08 20:30:15 +0300

    Add "Add Todo" & toggle

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Todo from "./components/Todo";
 import Todos from "./components/Todos";
@@ -10,7 +10,6 @@ const todoArr = [
 ];
 
 function App() {
-  console.log("App FUNCTION");
   const [todos, setTodos] = useState(todoArr);
 
   const onRemove = (id) => {
@@ -19,11 +18,42 @@ function App() {
 
   const [showTodos, setShowTodos] = useState(true);
 
+  let todoTitle = "";
+
+  // https://jsonplaceholder.typicode.com/todos
+  useEffect(() => {
+    console.log("AFTER FIRST RENDER");
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => res.json())
+      .then((json) => setTodos(json));
+    console.log("here");
+  }, []);
+
+  const onChange = (e) => {
+    todoTitle = e.target.value;
+  };
+
+  const addTodo = () => {
+    setTodos([
+      {
+        id: todos.length + 1,
+        title: todoTitle,
+        userId: 1,
+        completed: false,
+      },
+      ...todos,
+    ]);
+  };
+
   return (
     <div className="App">
-      <button>Add Todo</button>
-      {/* <button onClick={() => onRemove(2)}>Remove Todo id 2</button> */}
-      <button onClick={() => setShowTodos(false)}>Hide Todos</button>
+      input: <input type="text" onChange={onChange} />
+      <button onClick={addTodo}>Add Todo</button>
+      <br />
+      <br />
+      <button onClick={() => setShowTodos(!showTodos)}>
+        {showTodos ? "Hide" : "Show"} Todos
+      </button>
       <div>Todos Count: {todos.length}</div>
       <header className="App-header">
         {showTodos && todos.length > 0 ? (
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,10 +1,9 @@
+import "./Todo.css";
 function Todo({ id, children, onRemove }) {
   return (
     <div className="Todo">
-      <h1>
-        <button onClick={() => onRemove(id)}>X</button> MY TDDO ID: {id}{" "}
-        {children}
-      </h1>
+      <button onClick={() => onRemove(id)}>X</button> MY TDDO ID: {id}{" "}
+      {children}
     </div>
   );
 }
